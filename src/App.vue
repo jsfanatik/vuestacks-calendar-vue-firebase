fe0e5eb9adad24651aa2d1278883b9f47d5b7d94
jsfanatik <jsfanatik@gmail.com>
2019-08-31 11:06:25 -0400

    changed component name, deleted init.js

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <v-app>
     <v-content>
-      <HelloWorld/>
+      <Calendar/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Calendar from './components/Calendar';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Calendar,
   },
   data: () => ({
     //
