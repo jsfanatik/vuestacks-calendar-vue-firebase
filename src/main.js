288ecc5e791d8be5f6f2391e33c057715419d0e7
jsfanatik <jsfanatik@gmail.com>
2019-08-31 08:27:11 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,13 +9,13 @@ Vue.use(VueTextareaAutosize)
 Vue.config.productionTip = false
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCJ3J9D6KVyIjaMUReF8UspedH_8uzRXXw",
-  authDomain: "new-social-97999.firebaseapp.com",
-  databaseURL: "https://new-social-97999.firebaseio.com",
-  projectId: "new-social-97999",
-  storageBucket: "new-social-97999.appspot.com",
-  messagingSenderId: "795026422452",
-  appId: "1:795026422452:web:907bc11d8c98c57e"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 });
 
 export const db = firebase.firestore();
