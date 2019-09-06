436cb5deecdce678ef9786b420d5d4d23253c7b3
jsfanatik <49799116+jsfanatik@users.noreply.github.com>
2019-09-06 08:44:48 -0400

    Update Calendar.vue

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -46,48 +46,18 @@
         <v-card>
           <v-container>
             <v-form @submit.prevent="addEvent">
-              <v-text-field
-              v-model="name"
-              type="text"
-              label="event name (required)"
-              >
-            </v-text-field>
-            <v-text-field
-            v-model="details"
-            type="text"
-            label="detail"
-            >
-          </v-text-field>
-          <v-text-field
-          v-model="start"
-          type="date"
-          label="start (required)"
-          >
-        </v-text-field>
-        <v-text-field
-        v-model="end"
-        type="date"
-        label="end (required)"
-        >
-      </v-text-field>
-      <v-text-field
-      v-model="color"
-      type="color"
-      label="color (click to open color menu)"
-      >
-    </v-text-field>
-    <v-btn
-    type="submit"
-    color="primary"
-    class="mr-4"
-    @click.stop="dialog = false"
-    >
-    create event
-  </v-btn>
-</v-form>
-</v-container>
-</v-card>
-</v-dialog>
+              <v-text-field v-model="name" type="text" label="event name (required)"></v-text-field>
+              <v-text-field v-model="details" type="text" label="detail"></v-text-field>
+              <v-text-field v-model="start" type="date" label="start (required)"></v-text-field>
+              <v-text-field v-model="end" type="date" label="end (required)"></v-text-field>
+              <v-text-field v-model="color" type="color" label="color (click to open color menu)"></v-text-field>
+              <v-btn type="submit" color="primary" class="mr-4" @click.stop="dialog = false">
+                create event
+              </v-btn>
+            </v-form>
+          </v-container>
+        </v-card>
+      </v-dialog>
 
 <v-sheet height="600">
   <v-calendar
